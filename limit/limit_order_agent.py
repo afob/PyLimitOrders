15f90efc3a2f5f489f135340a3ec9d2117c62537
PASUPATHYRAM <pasupathyram1992@gmail.com>
2024-03-20 13:23:13 +0530

    Solution

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -1,5 +1,6 @@
-from trading_framework.execution_client import ExecutionClient
+from trading_framework.execution_client import ExecutionClient, ExecutionException
 from trading_framework.price_listener import PriceListener
+from collections import deque
 
 
 class LimitOrderAgent(PriceListener):
@@ -10,7 +11,29 @@ class LimitOrderAgent(PriceListener):
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
         super().__init__()
+        self.execution_client=execution_client
+        self.orders=deque()
 
     def on_price_tick(self, product_id: str, price: float):
         # see PriceListener protocol and readme file
-        pass
+        try:
+            if product_id=='IBM' and price<100:
+                self.execution_client.buy(product_id,1000)
+        except ExecutionException:
+            raise "Price greater than $100, unable to process"
+
+        for i in range(len(self.orders)):
+            ord_typ,ord_price,p_id,amount=self.orders.popleft()
+            if p_id==product_id and (ord_typ=='buy' and ord_price<100) or (ord_typ=='sell' and ord_price>=100):
+                try:
+                    if ord_typ=='buy':
+                        self.execution_client.buy(p_id,amount=amount)
+                    self.execution_client.sell(p_id,amount=amount)
+                except ExecutionException:
+                    raise "Not availble"
+    def add_orders(self,ord_typ: str, ord_price: float, p_id: str,amount: float):
+        self.orders.append((ord_typ,ord_price,p_id,amount))
+
+
+
+
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,23 @@
-import unittest
+from unittest import TestCase
+from unittest.mock import Mock
+from limit.limit_order_agent import LimitOrderAgent
 
-class LimitOrderAgentTest(unittest.TestCase):
+class LimitOrderAgentTest(TestCase):
 
     def test_something(self):
-        self.fail("not implemented")
+        self.execution_client=Mock()
+        self.tester=LimitOrderAgent(self.execution_client)
+
+    def test_add_orders(self):
+        #ord_typ: str, ord_price: float, p_id: str,amount: float
+        self.tester.orders('buy',100,'IBM',1000)
+        self.assertEqual(len(self.tester.orders),1)
+
+    def test_price_tik(self):
+        self.tester.orders('buy',100,'IBM',1000)
+        self.tester.on_price_tick('IBM',99)
+        self.execution_client.buy.assert_called_once_with('IBM',1000)
+
+
+
 
