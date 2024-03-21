4c3d388aefe27e700dfbd9227e196cc1cf0c396a
yadavravi316 <145346059+yadavravi316@users.noreply.github.com>
2024-03-21 23:27:01 +0530

    Update limit_order_agent_tests.py

--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,26 @@
 import unittest
+from limit.limit_order_agent import ExecutionClientSample , LimitOrderAgent
 
 class LimitOrderAgentTest(unittest.TestCase):
 
-    def test_something(self):
-        self.fail("not implemented")
+    def test_price_tick(self):
+        limobject = LimitOrderAgent(ExecutionClientSample())
+        testValue = limobject.on_price_tick('prod_1', 90)
+        message = "output value is not true."
+        self.assertTrue( testValue, message)
+
+    def test_add_order_buy(self):
+        limobject = LimitOrderAgent(ExecutionClientSample())
+        testValue_buy = limobject.add_order('B', 'prod_2', 50 , 10)
+        message = "output value is not true."
+        self.assertTrue( testValue_buy, message)
+
+
+    def test_add_order_buy(self):
+        limobject = LimitOrderAgent(ExecutionClientSample())
+        testValue_sell = limobject.add_order('S', 'prod_2', 15 , 10)
+        message = "output value is not true."
+        self.assertTrue( testValue_sell, message)
+
+
 
