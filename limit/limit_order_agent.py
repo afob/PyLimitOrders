62fb3e07584810fec24382c78dc373f02fc3d29f
yadavravi316 <145346059+yadavravi316@users.noreply.github.com>
2024-03-22 11:52:37 +0530

    Update limit_order_agent.py

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -22,21 +22,36 @@ class LimitOrderAgent(PriceListener):
         """
         super().__init__()
         self.execution_client = execution_client
+        self.added_orders = []
         
     def on_price_tick(self, product_id: str, price: float):
         # see PriceListener protocol and readme file
         # buy Product 1000 share if price is below 100
-        if(price < 100):
-            self.execution_client.buy(Product_id ,1000)
-         return True   
+        #Part 1
+        if(price < 100 and product_id == 'IBM'):
+            self.execution_client.buy(Product_id , 1000)
+            return True   
             
+        #part 2
+        for order_data in self.added_orders:
+            if(order_data['flag'] == 'B' and order_data['product_id']== product_id and order_data['limit'] >= price):
+                self.execution_client.buy(Product_id , order_data['amount'])
+               
+                self.added_orders.remove(order_data)
+                 
+                
+            elif(order_data['flag'] == 'S' and order_data['product_id']== product_id and order_data['limit'] <= price):
+                self.execution_client.sell(Product_id , order_data['amount'])
+                
+                self.added_orders.remove(order_data)
+                
+        return True
+                
     def add_order(self, flag : str, Product_id: str, amount :int, limit : int):
-        # based on the flag buy or sell , use of limit parameter is not understood hence not used 
-        if(flag == 'B'):
-            self.execution_client.buy(Product_id ,amount)
+        # based on the flag buy or sell added orders
+        if(flag == 'B' or flag == 'S'):
+            self.added_orders.append({'flag' : flag , 'Product_id'=Product_id , 'amount'=amount , 'limit' = limit})            
             return True
-        elif(flag == 'S'):
-            self.execution_client.sell(Product_id ,amount)
-            return True
+        
 
 
