1c459bef4ef0795443011c3d517b0bd632707dbd
yadavravi316 <145346059+yadavravi316@users.noreply.github.com>
2024-03-26 19:54:56 +0530

    Update limit_order_agent_tests.py

--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,27 +1,28 @@
 import unittest
-from limit.limit_order_agent import ExecutionClientSample , LimitOrderAgent
+from limit.limit_order_agent import LimitOrderAgent
+from trading_framework.execution_client import ExecutionClient
 
 class LimitOrderAgentTest(unittest.TestCase):
 
-    def test_price_tick(self):
-        limobject = LimitOrderAgent(ExecutionClientSample())
-        testValue = limobject.on_price_tick('IBM', 90)
-        message = "output value is not true."
-        self.assertTrue( testValue, message)
+    def setUp(self):
+        self.limitagent = LimitOrderAgent(ExecutionClient())   
+        
+    def test_price_tick(self):        
+        (pid,price) = self.limitagent.on_price_tick('IBM', 90)
+        self.assertEqual(('IBM', 90),(pid,price))    
 
-    def test_add_order_buy(self):
-        limobject = LimitOrderAgent(ExecutionClientSample())
-        testValue_buy = limobject.add_order('B', 'prod_1', 50 , 10)
-        message = "output value is not true."
-        self.assertTrue( testValue_buy, message)
-
+    
+    def test_add_order_buy(self):        
+        testadd = self.limitagent.add_order('B', 'Prod_1', 500 , 80)
+        testbuy = self.limitagent.execute_added_orders()
+        self.assertEqual(testadd, testbuy)
 
     def test_add_order_sell(self):
-        limobject = LimitOrderAgent(ExecutionClientSample())
-        testValue_sell = limobject.add_order('S', 'prod_2', 15 , 10)
-        message = "output value is not true."
-        self.assertTrue( testValue_sell, message)
+        testAdd_sell = self.limitagent.add_order('S', 'Prod_2', 150 , 110)
+        testsell = self.limitagent.execute_added_orders()
+        self.assertEqual(testValue_sell, testsell)
 
 
 if __name__ == "__main__":
     unittest.main()
+    
