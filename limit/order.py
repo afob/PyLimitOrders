45d7a941058ba42f61b407e54c9edd617bb8bd7b
pooja-mittal068 <pooja.mittal068@gmail.com>
2024-03-26 18:23:16 +0530

    PyOrderLimit updated

--- limit/order.py
+++ limit/order.py
@@ -5,8 +5,10 @@ from flag import FLAG
 
 class Order(LimitOrderAgent):
 
-    def place_order(self, product_id: str, price_limit: float, units: int, flag : FLAG):
-        if units > 0 :
-            LimitOrderAgent.add_order(product_id, price_limit, units*price_limit)
-            return True
-        return False
+    # product_details = [{product_id: str, price_limit: float, units: int, flag :FLAG},{product_id: str, price_limit: float, units: int, flag :FLAG}]
+    def place_order(self, product_details:list):
+        for product_detail in product_details:
+            if product_detail['units'] > 0 :
+                LimitOrderAgent.add_order(product_detail['flag'],product_detail['product_id'], product_detail['price_limit'], product_detail['units']*product_detail['price_limit'])
+        return True    
+
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -17,15 +17,16 @@ class OrderTest(unittest.TestCase):
     def test_place_order_buy(self):
         thing = ExecutionClient()
         thing.buy = MagicMock(return_value=None)
-        self.assertTrue(Order.place_order('IBM', 100.0, 1000, FLAG.BUY))
+        self.assertTrue(Order.place_order([{product_id: 'IBM', price_limit: 100.0, units: 1000, flag :True},{product_id: 'TATA', price_limit: 50.0, units: 500, flag :True}]))
+        
 
     def test_place_order_buy_1(self):
         thing = ExecutionClient()
         thing.buy = MagicMock(return_value=None)
-        self.assertFalse(Order.place_order('IBM', 100.0, 0, FLAG.BUY))
+        self.assertFalse(Order.place_order([{product_id: 'IBM', price_limit: 100.0, units: 1000, flag :False},{product_id: 'TATA', price_limit: 50.0, units: 500, flag :False}]))
 
     def test_place_order_sell(self):
         thing = ExecutionClient()
         thing.buy = MagicMock(return_value=None)
-        self.assertTrue(Order.place_order('IBM', 150.0, 100, FLAG.SELL))
+        self.assertTrue(Order.place_order([{product_id: 'Honda', price_limit: 1400.0, units: 2500, flag :True},{product_id: 'NIFTY', price_limit: 500.0, units: 2500, flag :True}]))
 
