9c9e49eaf3b88ca807c40bb3464b709f85e4ca72
yadavravi316 <145346059+yadavravi316@users.noreply.github.com>
2024-03-26 20:21:02 +0530

    Update limit_order_agent_tests.py

--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -2,15 +2,22 @@ import unittest
 from limit.limit_order_agent import LimitOrderAgent
 from trading_framework.execution_client import ExecutionClient
 
+class ExecutionClientSample(ExecutionClient):
+    def buy(self, product_id ,amount):
+        print(f"purchased {product_id}")
+
+    def sell(self, product_id ,amount):
+        print(f"sold {product_id}")
+
 class LimitOrderAgentTest(unittest.TestCase):
 
     def setUp(self):
-        self.limitagent = LimitOrderAgent(ExecutionClient())   
+        self.limitagent = LimitOrderAgent(ExecutionClientSample())   
+        
         
     def test_price_tick(self):        
         (pid,price) = self.limitagent.on_price_tick('IBM', 90)
-        self.assertEqual(('IBM', 90),(pid,price))    
-
+        self.assertEqual(('IBM', 90),(pid,price))  
     
     def test_add_order_buy(self):        
         testadd = self.limitagent.add_order('B', 'Prod_1', 500 , 80)
