582a3a6b173ecbc5fa3cf43dc86f381e01d793c3
sairam2416 <165991729+sairam2416@users.noreply.github.com>
2024-04-05 11:59:52 +0530

    Update limit_order_agent_tests.py

--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,37 @@
 import unittest
+from limit.limit_order_agent import LimitOrderAgent
+from trading_framework.execution_client import ExecutionClient
+
 
 class LimitOrderAgentTest(unittest.TestCase):
 
-    def test_something(self):
-        self.fail("not implemented")
+    def setUp(self):
+        self.execution_client_mock = ExecutionClient
+        self.limit_order_agent = LimitOrderAgent(self.execution_client_mock)
 
+    def test_add_order(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 99.5)
+        self.assertEqual(len(self.limit_order_agent.orders), 1)
+        print("test_add_order Passed Successfully")
+
+    def test_execute_buy_order(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100.5)
+        self.limit_order_agent.on_price_tick('IBM', 99.0)
+        self.assertTrue(self.limit_order_agent.orders)
+        print("test_execute_buy_order Passed Successfully")
+
+    def test_execute_sell_order(self):
+        self.limit_order_agent.add_order('sell', 'IBM', 1000, 101.0)
+        self.limit_order_agent.on_price_tick('IBM', 102.0)
+        self.assertFalse(self.limit_order_agent.orders)
+        print("test_execute_sell_order Passed Successfully")
+
+    def test_execute_wrong_order(self):
+        self.limit_order_agent.add_order('wrong', 'IBM', 1000, 101.0)
+        self.limit_order_agent.price_tick('IBM', 102.0)
+        self.assertTrue(self.limit_order_agent.orders)
+        print("test_execute_sell_order Passed Successfully")
+
+
+if __name__ == '__main__':
+    unittest.main()
