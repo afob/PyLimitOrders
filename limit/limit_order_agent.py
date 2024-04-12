c194242f1a8b7b6baac2c6bd475149488adbeb37
priyankanesargi21 <priyankanesargi@gmail.com>
2024-04-12 23:04:34 +0530

    limit order functionality

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -10,7 +10,59 @@ class LimitOrderAgent(PriceListener):
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
         super().__init__()
+        self.execution_client = execution_client
+        self.orders = []
 
     def on_price_tick(self, product_id: str, price: float):
         # see PriceListener protocol and readme file
-        pass
+        # Implement LimitOrderAgent such that it buys 1000 shares of IBM when the price drops below $100
+        # if product_id == 'IBM' and float(price) < float(100):
+        #     self.execution_client.buy(product_id, 1000)
+        return self.execute_order(product_id, price)
+
+    def add_order(self, flag: str, product_id: str, amount: int, limit: int):
+        """
+        method to accept order
+        :param flag: indicates whether to buy or sell
+        :param product_id: the product to buy
+        :param amount: the amount to buy
+        :param limit: the limit at which to buy or sell
+        Author: Priyanka Nesargi
+        """
+        self.orders.append({'flag': flag, 'product_id': product_id, 'amount': amount, 'limit': limit})
+        print(self.orders, "order added successfully")
+        return True
+
+    def execute_order(self, product_id: str, price: float):
+        """
+        method to buy or sell order
+        :param product_id: the product to buy
+        :param price: the current market price of the product
+        Author: Priyanka Nesargi
+        """
+        # orders consists only orders which of requested product id reducing looping 
+        orders = list(filter(lambda x: x.get('product_id', '')==product_id, self.orders))
+        success = False
+        print(orders)
+        for order in orders:
+            if order['flag'] == 'buy' and float(price) <= float(order['limit']):
+                self.execution_client.buy(self,order['product_id'], order['amount'])
+                self.orders.remove(order)
+                success = True
+                print(self.orders, "clearing")
+                print("{0} amount of {1} bought successfully at {2} price".format(order['amount'], order['product_id'], price))
+            elif order['flag'] == 'sell' and float(price) >= float(order['limit']):
+                self.execution_client.sell(self,order['product_id'], order['amount'])
+                # self.orders.pop(self.orders.index(order))
+                self.orders.remove(order)
+                success = True
+                print("{0} amount of {1} sold successfully at {2} price".format(order['amount'], order['product_id'], price))
+            else:
+                success = False
+        return success
+
+
+
+
+        
+        
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,34 @@
 import unittest
+from trading_framework.execution_client import ExecutionClient
+from limit.limit_order_agent import LimitOrderAgent
+
 
 class LimitOrderAgentTest(unittest.TestCase):
 
-    def test_something(self):
-        self.fail("not implemented")
+    def setUp(self):
+        self.execution_client_mock = ExecutionClient
+        self.limit_order_agent = LimitOrderAgent(self.execution_client_mock)
+
+    def test_add_order(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100)
+        self.assertEqual(len(self.limit_order_agent.orders), 1)
+        print("test_add_order Passed Successfully")
+
+    def test_execute_buy_order(self):
+        self.limit_order_agent.add_order('buy', 'IBM', 1000, 100)
+        res = self.limit_order_agent.on_price_tick('IBM', 99.0)
+        self.assertTrue(res)
+        print("test_execute_buy_order Passed Successfully")
+
+    def test_execute_sell_order(self):
+        self.limit_order_agent.add_order('sell', 'IBM', 1000, 125.0)
+        res = self.limit_order_agent.on_price_tick('IBM', 130.0)
+        self.assertTrue(res)
+        print("test_execute_sell_order Passed Successfully")
+
+    def test_execute_bought_order(self):
+        res = self.limit_order_agent.add_order('bought', 'IBM', 1000, 1001.0)
+        self.assertTrue(res)
+        print("test_execute_bought_order Passed Successfully")
+
 
