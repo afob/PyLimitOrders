458452598f2e10f7c28391402405beaa939b6d73
priyankanesargi21 <priyankanesargi@gmail.com>
2024-04-19 17:54:07 +0530

    removing of order changes

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -30,7 +30,7 @@ class LimitOrderAgent(PriceListener):
         """
         try:
             if flag in ['buy', 'sell']:
-                self.orders.append({'flag': flag, 'product_id': product_id, 'amount': amount, 'limit': limit})
+                self.orders.append({'flag': flag, 'product_id': product_id, 'amount': amount, 'limit': limit, 'is_executed':False})
                 print(self.orders, "order added successfully")
                 return True
             else:
@@ -51,26 +51,29 @@ class LimitOrderAgent(PriceListener):
             # Implement LimitOrderAgent such that it buys 1000 shares of IBM when the price drops below $100
             # if product_id == 'IBM' and float(price) < float(100):
             #     self.execution_client.buy(product_id, 1000)
-            
             price = self.on_price_tick(product_id, price)
             # orders consists only orders which are of requested product id reducing looping and checking if the proudct id matches with requested product id so to reduce loop i have used filter 
-            orders = list(filter(lambda x: x.get('product_id', '')==product_id, self.orders))
+            # orders = list(filter(lambda x: x.get('product_id', '')==product_id, self.orders))
+            orders = list(filter((lambda x:x.get('product_id', '')==product_id and x.get('is_executed')==False), self.orders))
             print(orders)
             for order in orders:
-                print(order)
                 if order['flag'] == 'buy' and float(price) <= float(order['limit']):
                     self.execution_client.buy(self,order['product_id'], order['amount'])
-                    self.orders.remove(order)
-                    print(self.orders, "clearing")
-                    print("{0} amount of {1} bought successfully at {2} price".format(order['amount'], order['product_id'], price))
+                    order['is_executed'] = True
+                    print(order)
+                    print("{0} amount of {1} bought successfully at {2} price".format(order['amount'], order['product_id'], price))   
                 elif order['flag'] == 'sell' and float(price) >= float(order['limit']):
                     self.execution_client.sell(self,order['product_id'], order['amount'])
-                    # self.orders.pop(self.orders.index(order))
-                    self.orders.remove(order)
+                    order['is_executed'] = True
+                    print(order)
                     print("{0} amount of {1} sold successfully at {2} price".format(order['amount'], order['product_id'], price))
                 else:
                     print("{0} amount of {1} does not satisfy {2} price which was placed at limit {3}".format(order['amount'], order['product_id'], price, order['limit']))
+            # if required to remove from list then we can remove orders which ever is_executed status is True
+            # here i am filtering the orders list and storing only orders which have is_executed status False
+            # self.orders = list(filter(lambda x: x.get('is_executed', '')==False, self.orders))
             return True
+        
         except Exception as e:
             print(str(e))
             raise ExecutionException
