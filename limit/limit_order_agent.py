51a0b58b7a921bbbc76c1d88c125381a4f7d80e5
Ankit Verma <ankitver090@gmial.com>
2024-08-28 09:01:52 +0530

    implementation of limit order system

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -1,16 +1,46 @@
-from trading_framework.execution_client import ExecutionClient
+from trading_framework.execution_client import ExecutionClient, ExecutionException
 from trading_framework.price_listener import PriceListener
-
 
 class LimitOrderAgent(PriceListener):
 
     def __init__(self, execution_client: ExecutionClient) -> None:
+        self.execution_client = execution_client
+        self.orders = [] # Queue for storing orders.
+    def add_order(self,type, pid, amt, limit):
         """
-
-        :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
+        Add the order in the queue to execute it on the basis of triggers.
         """
-        super().__init__()
+        if type not in {'buy', 'sell'}:
+            raise ValueError("Type for the order must be either 'buy' or 'sell'.")
+        if amt <= 0:
+            raise ValueError("Amount must be greater than 0.")
+        if limit <= 0:
+            raise ValueError("Limit should be greater than 0.") 
+        self.orders.append({'type':type, 'pid':pid, 'amt':amt, 'limit':limit})
+        print(f"Order {(type, pid, amt, limit)} Added Successfully.")
 
+        
     def on_price_tick(self, product_id: str, price: float):
-        # see PriceListener protocol and readme file
-        pass
+        """
+        execute the orders in the queue for based on the type and limits.
+        """
+        completed_order = []
+        for order in self.orders:
+            if order['pid'] == product_id:
+                try:
+                    if order['type'] == 'buy' and order['limit'] >= price:
+                        self.execution_client.buy(product_id, order['amt'])
+                        completed_order.append(order)
+                    if order['type'] == 'sell' and order['limit'] <= price:
+                        self.execution_client.sell(product_id, order['amt'])
+                        completed_order.append(order)
+                except Exception as e:
+                    raise ExecutionException(f"Failed to execute {order['type']} order for {order['pid']}.")
+                
+                for order in completed_order:
+                    self.orders.remove(order) # Removing the orders from the queue after successfully execution.
+                
+            
+
+
+                
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,49 @@
 import unittest
 
-class LimitOrderAgentTest(unittest.TestCase):
+from limit.limit_order_agent import LimitOrderAgent
+class MockExecutionClient:
+    def __init__(self) -> None:
+        self.executed_order = []
+    
+    def buy(self,order_type, amt):
+        self.executed_order.append((order_type, amt, 'buy'))
+    
+    def sell(self,order_type, amt):
+        self.executed_order.append((order_type, amt, 'sell'))
+class Test(unittest.TestCase):
+    client = MockExecutionClient()
+    def test_buy(self):
+        #adding order
+        print(1)
+        agent = LimitOrderAgent(self.client)
+        agent.add_order('buy', 'IBM', 1000, 100)
+        agent.on_price_tick('IBM', 99)
+        self.assertIn(('IBM', 1000, 'buy'), self.client.executed_order)
+        self.assertEqual(len(agent.orders), 0)
+    
+    def test_sell(self):
+        print(2)
+        agent = LimitOrderAgent(self.client)
+        agent.add_order('sell', 'IBM', 500, 150)
+        agent.on_price_tick('IBM', 151)
+        self.assertIn(('IBM', 500, 'sell'), self.client.executed_order)
+        self.assertEqual(len(agent.orders), 0)
 
-    def test_something(self):
-        self.fail("not implemented")
+    def test_buy_negative(self):
+        #adding order
+        agent = LimitOrderAgent(self.client)
+        agent.add_order('buy', 'APPL', 1000, 100)
+        agent.on_price_tick('APPL', 101)
+        self.assertNotIn(('APPL', 1000, 'buy'), self.client.executed_order)
+        self.assertEqual(len(agent.orders), 1)
+    
+    def test_sell_negative(self):
+        agent = LimitOrderAgent(self.client)
+        agent.add_order('sell', 'GOOGL', 500, 150)
+        agent.on_price_tick('GOOGL', 110)
+        self.assertNotIn(('GOOGL', 500, 'sell'), self.client.executed_order)
+        self.assertEqual(len(agent.orders), 1)
 
+if __name__ == "__main__":
+    unittest.main()
+    
