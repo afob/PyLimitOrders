3a9e3b2448af85c7faae912033edfd2d7b0e4fae
Aadarsh Aravind <aadarsh1091@gmail.com>
2024-09-05 19:23:11 +0530

    Pylint_problem_statement

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -10,7 +10,48 @@ class LimitOrderAgent(PriceListener):
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
         super().__init__()
+        self.orders=[]
+        self.execution_client = execution_client
+        
+    def add_order(self,buy:bool,product_id:str,amount:int,price_limit:float):
+        """
+        order's to be added
+        :param by: a flag to indicate whether to buy or sell
+        :param product: ID of the product
+        :param amount: amount to buy/sell
+        :price limit: price limit of product to buy/sell
+        """
+        try:
+        
+            order_dict={"is_buy":buy,"prod_id":product_id,"price":amount,"price_limit":price_limit}
+            self.orders.append(order_dict)
+            
+        except Exception as e:
+        
+            print(f"Exception at add_order : {e}")
 
     def on_price_tick(self, product_id: str, price: float):
-        # see PriceListener protocol and readme file
-        pass
+        """
+        Called whenever we need to updat price for the product.
+        :param product_id: PRODUCT ID
+        :param price: Price of the product
+        """
+        try:
+        
+            for order in list(self.orders):
+                
+                if order["prod_id"] == product_id:
+                
+                    if order["is_buy"] and price<=order["price_limit"]:
+                    
+                        self.execution_client.buy(product_id,order["price"])
+                        self.orders.remove(order)
+                        
+                    elif (not order["is_buy"]) and price>=order["price_limit"]:
+                    
+                        self.execution_client.buy(product_id,order["sell"])
+                        self.orders.remove(order)
+                        
+        except Exception as e:
+        
+            print(f"Exception at on_price_tick : {e}")
--- limit/tests/limit_order_agent_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-class LimitOrderAgentTest(unittest.TestCase):
-
-    def test_something(self):
-        self.fail("not implemented")
-
