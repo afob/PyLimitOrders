3b6c2f5f9fe60152af796e8c346958d21e58dea0
Rajesh Kumar Sahoo <rajesh2012acs@gmail.com>
2024-09-09 17:59:39 +0530

    test cases change

--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -10,7 +10,7 @@ class LimitOrderAgentTest(unittest.TestCase):
         LOAgent = LimitOrderAgent(exec_client)
 
         # Buy order for IBM
-        LOAgent.add_order(is_buy=True,product_id="IBM",
+        LOAgent.add_orders(is_buy=True,product_id="IBM",
         amount=1000,price_limit=100)
 
         #Price is above the limit, order should not execute
@@ -18,7 +18,7 @@ class LimitOrderAgentTest(unittest.TestCase):
         self.assertFalse(res)
 
         #Price is below the limit, order should execute
-        LOAgent.on_price_tick(product_id="IBM",price=80)
+        res = LOAgent.on_price_tick(product_id="IBM",price=80)
         self.assertTrue(res)
     
 
@@ -35,7 +35,7 @@ class LimitOrderAgentTest(unittest.TestCase):
         self.assertFalse(res)
 
         #Price above the limit, order should execute
-        LOAgent.on_price_tick(product_id="NVIDIA",price=151)
+        res = LOAgent.on_price_tick(product_id="NVIDIA",price=151)
         self.assertTrue(res)
 
 
